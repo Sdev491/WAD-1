1f11d81fe7262ac379fa7054a0bb7042bfdd2466
awebstudent <126592792+awebstudent@users.noreply.github.com>
2023-05-24 17:42:02 +0530

    Update nodecode.js

--- nodecode.js
+++ nodecode.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(req,res){
-  res.send("<h1>Hello, World!</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/contact",function(req,res){
