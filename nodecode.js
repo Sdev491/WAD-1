0e6604adf85862533c95170b7ab0aa5025ac1cb6
awebstudent <126592792+awebstudent@users.noreply.github.com>
2023-05-24 15:16:35 +0530

    pahila wad

---
+++ nodecode.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/",function(req,res){
+  res.send("<h1>Hello, World!</h1>");
+});
+
+app.get("/contact",function(req,res){
+  res.send("Contact me -_-");
+});
+
+app.get("/about",function(req,res){
+  res.send("I am an undergraduate student currently studying in 3rd Year of my IT major.");
+});
+
+app.listen(3000,function(){
+  console.log("server started on port 3000");
+});
